006234d814e206d7cd55f6ac2b3a050e35125034
MattieShoes <no@thanks.com>
2023-12-16 09:27:02 -0700

    Cleaned up, removed recursion

--- 16.py
+++ 16.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
 
-import sys
-
-# moves beam of light recursively
-def recurse(r, c, d, e):
+# finds the next tile(s) the beam will travel to
+def do_tile(r, c, d, e):
     if r>=height or c>=width or r<0 or c<0 or (r,c) in e[d]:
-        return
+        return []
     e[d].add((r,c))
     tile = lines[r][c]
     if tile == '.':
-        recurse(r+d[0], c+d[1], d, e)
+        return([[r+d[0], c+d[1], d]])
     elif tile == '\\':
-        recurse(r+d[1], c+d[0], (d[1], d[0]), e)
+        return([[r+d[1], c+d[0], (d[1], d[0])]])
     elif tile == '/':
-        recurse(r-d[1], c-d[0], (-d[1], -d[0]), e)
+        return([[r-d[1], c-d[0], (-d[1], -d[0])]])
     elif tile == '-':
         if d[1] != 0:
-            recurse(r+d[0], c+d[1], d, e)
+            return([[r+d[0], c+d[1], d]])
         else:
-            recurse(r, c-1, (0, -1), e)
-            recurse(r, c+1, (0, 1), e)
+            return([[r, c-1, (0, -1)],[r, c+1, (0, 1)]])
     elif tile == '|':
         if d[0] != 0:
-            recurse(r+d[0], c+d[1], d, e)
+            return([[r+d[0], c+d[1], d]])
         else:
-            recurse(r-1, c, (-1, 0), e)
-            recurse(r+1, c, (1, 0), e)
+            return([[r-1, c, (-1, 0)], [r+1, c, (1, 0)]])
 
-# gets the number of energized tiles
+# gets the number of energized tiles given a start point
 def calculate(row, col, direction):
     e = {(0,1): set(), (1,0): set(), (-1,0): set(), (0,-1): set()}
-    recurse(row, col, direction, e)
+    tbs = [[row, col, direction]]
+    while len(tbs) > 0:
+        tbs.extend(do_tile(tbs[0][0], tbs[0][1], tbs[0][2], e))
+        del tbs[0]
     return len(e[(0,1)].union(e[(1,0)], e[(-1,0)], e[(0,-1)])) 
 
 with open("inputs/16") as f:
@@ -38,7 +37,6 @@ with open("inputs/16") as f:
 height = len(lines)
 width = len(lines[0])
 
-sys.setrecursionlimit(10000)
 part1 = calculate(0,0,(0,1))
 print(f"Part 1: {part1}")
 
