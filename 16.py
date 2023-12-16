9cf7a0b70d88e46f4b171cb9ac70d763d17a4f33
MattieShoes <no@thanks.com>
2023-12-16 12:45:51 -0700

    Cleaned up

--- 16.py
+++ 16.py
@@ -1,36 +1,50 @@
 #!/usr/bin/python3
 
-# finds the next tile(s) the beam will travel to
-def do_tile(r, c, d, e):
-    if r>=height or c>=width or r<0 or c<0 or (r,c) in e[d]:
+from collections import namedtuple
+
+# Queue entry contains row, column, and a direction tuple
+Node = namedtuple('Node', ['row', 'col', 'd'])
+
+# adds to energized and visited, returns Nodes the beam will travel to
+def expand_node(n, visited, energized):
+    # bail if beam left grid
+    if n.row >= height or n.col >= width or n.row < 0 or n.col < 0:
         return []
-    e[d].add((r,c))
-    tile = lines[r][c]
+    # bail if we've already expanded an identical node
+    if n in visited:
+        return []
+    # add to visited and energized 
+    visited.add(n)
+    energized.add((n.row, n.col))
+    tile = lines[n.row][n.col]
+    # return continuation nodes
     if tile == '.':
-        return([[r+d[0], c+d[1], d]])
+        return([Node(n.row + n.d[0], n.col + n.d[1], n.d)])
     elif tile == '\\':
-        return([[r+d[1], c+d[0], (d[1], d[0])]])
+        return([Node(n.row + n.d[1], n.col + n.d[0], (n.d[1], n.d[0]))])
     elif tile == '/':
-        return([[r-d[1], c-d[0], (-d[1], -d[0])]])
+        return([Node(n.row - n.d[1], n.col - n.d[0], (-n.d[1], -n.d[0]))])
     elif tile == '-':
-        if d[1] != 0:
-            return([[r+d[0], c+d[1], d]])
+        if n.d[1] != 0:
+            return([Node(n.row + n.d[0], n.col + n.d[1], n.d)])
         else:
-            return([[r, c-1, (0, -1)],[r, c+1, (0, 1)]])
+            return([Node(n.row, n.col-1, (0, -1)), Node(n.row, n.col+1, (0,1))])
     elif tile == '|':
-        if d[0] != 0:
-            return([[r+d[0], c+d[1], d]])
+        if n.d[0] != 0:
+            return([Node(n.row + n.d[0], n.col + n.d[1], n.d)])
         else:
-            return([[r-1, c, (-1, 0)], [r+1, c, (1, 0)]])
+            return([Node(n.row-1, n.col, (-1, 0)), Node(n.row+1, n.col, (1,0))])
 
 # gets the number of energized tiles given a start point
 def calculate(row, col, direction):
-    e = {(0,1): set(), (1,0): set(), (-1,0): set(), (0,-1): set()}
-    tbs = [[row, col, direction]]
-    while len(tbs) > 0:
-        tbs.extend(do_tile(tbs[0][0], tbs[0][1], tbs[0][2], e))
-        del tbs[0]
-    return len(e[(0,1)].union(e[(1,0)], e[(-1,0)], e[(0,-1)])) 
+    energized = set()
+    visited = set()
+    queue = [Node(row, col, direction)]
+    # expand nodes from queue until queue is empty
+    while len(queue) > 0:
+        queue.extend(expand_node(queue[0], visited, energized))
+        del queue[0]
+    return(len(energized))
 
 with open("inputs/16") as f:
     lines = f.read().rstrip("\n").split("\n")
@@ -42,13 +56,9 @@ print(f"Part 1: {part1}")
 
 part2 = 0
 for r in range(height):
-    for c in range(width):
-        if r == 0:
-            part2 = max(part2, calculate(r, c, (1,0)))
-        elif r == height-1:
-            part2 = max(part2, calculate(r, c, (-1,0)))
-        if c == 0:
-            part2 = max(part2, calculate(r, c, (0,1)))
-        elif c == width-1:
-            part2 = max(part2, calculate(r, c, (0,-1)))
+    part2 = max(part2, calculate(r, 0, (0, 1)))
+    part2 = max(part2, calculate(r, width-1, (0, -1)))
+for c in range(width):
+    part2 = max(part2, calculate(0, c, (1, 0)))
+    part2 = max(part2, calculate(height-1, c, (-1, 0)))
 print(f"Part 2: {part2}")
